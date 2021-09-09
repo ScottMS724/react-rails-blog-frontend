cae5afaa5a200c7b24cf982cbf6216292f7eda97
scottms724 <scottms724@gmail.com>
2021-09-09 12:44:40 -0400

    first commit, dependencies and stubbed routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route, withRouter, Switch } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/posts" component={Posts} />
+            <Route exact path="/posts/:id" component={Post} />
+            <Route exact path="/new_post" component={NewPost} />
+          </Switch>
+      </div>
+    )
+  }
+
 }
 
 export default App;
